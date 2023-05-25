b4da3db80431ba9aeb87a0f91057409d789048d4
Tiago Silva <tiagofef@gmail.com>
2023-05-25 09:03:50 -0300

    Adicionando h2 e criacao da base de dados

--- src/main/java/com/tiago/cursomc/domain/Categoria.java
+++ src/main/java/com/tiago/cursomc/domain/Categoria.java
@@ -1,10 +1,20 @@
 package com.tiago.cursomc.domain;
 
+import jdk.jfr.Name;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
-
+@Entity
 public class Categoria implements Serializable {
+    private static final long serialVersionUID = 1l;
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
+
     private String nome;
 
     public Categoria() {
